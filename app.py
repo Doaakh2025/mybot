3cafa112eee1e5a69ab6a3cfa833db5447d7cc02
Doaakh2025 <doaakhaled008@gmail.com>
2025-05-22 00:22:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import requests
-import streamlit as st 
+import streamlit as st
 
-dify_api_key = "app-RUWoWFhQZp6yg5OFr6bVc7if"
+
+dify_api_key = "app-VhMO2jC4F1CrmwpO9cEk8E6s"
+
+
 url = "https://api.dify.ai/v1/chat-messages"
 
 st.title("Dify Streamlit App")
 
+
 if "conversation_id" not in st.session_state:
     st.session_state.conversation_id = ""
-    
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -16,12 +20,15 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-prompt = st.chat_input("Enter you question")
+
+prompt = st.chat_input("Enter your question")
 
 if prompt:
+  
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
+
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
@@ -34,9 +41,9 @@ if prompt:
         payload = {
             "inputs": {},
             "query": prompt,
-            "response_mode": "blocking",
+            "response_mode": "blocking",  # أو "streaming" حسب التطبيق
             "conversation_id": st.session_state.conversation_id,
-            "user": "aianytime",
+            "user": "test_user",
             "files": []
         }
 
@@ -45,7 +52,10 @@ if prompt:
             response.raise_for_status()
             response_data = response.json()
 
-            full_response = response_data.get('answer', '')
+ 
+            # st.write(response_data)
+
+            full_response = response_data.get('answer', 'No answer found.')
             new_conversation_id = response_data.get('conversation_id', st.session_state.conversation_id)
             st.session_state.conversation_id = new_conversation_id
 
@@ -54,5 +64,4 @@ if prompt:
             full_response = "An error occurred while fetching the response."
 
         message_placeholder.markdown(full_response)
-
         st.session_state.messages.append({"role": "assistant", "content": full_response})
