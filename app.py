71893416ce5c33d7287ffc37479876a746dc36ec
Doaakh2025 <doaakhaled008@gmail.com>
2025-05-22 00:46:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,42 @@
 import requests
-import streamlit as st
-dify_api_key = "app-0zVjkv5iC1neGpNPhNAQabS5"
+import streamlit as st 
 
-curl -X POST 'https://api.dify.ai/v1/chat-messages' \
---header 'Authorization: Bearer {api_key}' \
---header 'Content-Type: application/json' \
+dify_api_key = "app-RUWoWFhQZp6yg5OFr6bVc7if"
+url = "https://api.dify.ai/v1/chat-messages"
 
+st.title("Medical chatbot")
 
-st.title("Dify Streamlit App")
 if "conversation_id" not in st.session_state:
     st.session_state.conversation_id = ""
-
+    
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-prompt = st.chat_input("Enter your question")
+
+prompt = st.chat_input("Enter you question")
 
 if prompt:
-  
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
-
 
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
 
         headers = {
-            'Authorization': 'Bearer app-0zVjkv5iC1neGpNPhNAQabS5',
-
+            'Authorization': f'Bearer {dify_api_key}',
             'Content-Type': 'application/json'
         }
 
         payload = {
             "inputs": {},
             "query": prompt,
-            "response_mode": "blocking",  # أو "streaming" 
+            "response_mode": "blocking",
             "conversation_id": st.session_state.conversation_id,
-            "user": "test_user",
+            "user": "aianytime",
             "files": []
         }
 
@@ -49,10 +45,7 @@ if prompt:
             response.raise_for_status()
             response_data = response.json()
 
- 
-            # st.write(response_data)
-
-            full_response = response_data.get('answer', 'No answer found.')
+            full_response = response_data.get('answer', '')
             new_conversation_id = response_data.get('conversation_id', st.session_state.conversation_id)
             st.session_state.conversation_id = new_conversation_id
 
@@ -61,4 +54,5 @@ if prompt:
             full_response = "An error occurred while fetching the response."
 
         message_placeholder.markdown(full_response)
+
         st.session_state.messages.append({"role": "assistant", "content": full_response})
